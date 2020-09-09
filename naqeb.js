d53976bf3e51c47f22dc82da44bc0d10c20a7b82
hamahormz <70761216+hamahormz@users.noreply.github.com>
2020-09-09 16:36:59 +0300

    Update naqeb.js

--- naqeb.js
+++ naqeb.js
@@ -4,7 +4,7 @@ const invites = {};
 const wait = require("util").promisify(setTimeout);
 const moment = require("moment");
 const fs = require("fs");
-var prefix = "!";
+var prefix = "N/";
 
 naqeb.on("ready", () => {
   console.log(`Logged in as ${naqeb.user.tag}!`);
